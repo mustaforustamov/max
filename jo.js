da6e8b4875f1761db2bbc1f34e3ec614acabd97c
mustaforustamov <mustaforustamov733@gmail.com>
2025-05-19 15:02:15 +0500

    Add files via upload

---
+++ jo.js
@@ -0,0 +1,28 @@
+const askName = prompt('ismingiz nima');
+
+
+const askSurname = prompt('familiyangiz nima');
+
+
+const askAge = prompt('yoshingiz nechida');
+
+const askSubject= prompt('qaysi fanni yoqtirasiz');
+
+const ask= prompt('yashash tumaningiz');
+alert("ma'lumotlaringizni kiritganingiz uchun tashakkur")
+
+
+console.log("Ismingiz: "+ askName)
+console.log("Familyangiz: "+ askSurname)
+console.log("Yoshingiz: "+ askAge)
+console.log("Yoqtirgan faningiz: "+ askSubject)
+console.log("Tumaningiz: " + ask)
+
+
+
+
+console.log(a)
+console.log("Tumaningiz: " + ask)
+console.log(firstName)
+
+console.log(confirm("javobni tanlang"))
